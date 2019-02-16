d8eedd14641c432c0c51eaef8e31d6ea81f1c822
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-02-16 23:21:03 +0530

    chore(setup): add prettier, husky, lint-staged, eslint, emotion

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const App = () => (
-  <h1>Hello</h1>
-)
+import App from './App'
 
 render(<App />, document.getElementById('root'))
-
