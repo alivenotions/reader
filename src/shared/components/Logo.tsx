4c595e101d9e6460f612923dbbc12c04ddc1eb72
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-04-03 06:02:36 +0530

    feat(svg): add Logo and RSSFeedIcon components

---
+++ src/shared/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * TODO: Make this reusable and more general
+ */
+function Logo() {
+  return (
+    <span className="text-teal-dark pl-1 pr-2 text-5xl border-teal-darkest border-solid border-4 tracking-tight font-display font-bold">
+      reader.
+    </span>
+  )
+}
+
+export default Logo
