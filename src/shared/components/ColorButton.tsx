f17ce6d26f80d4ab7e7c37374896cb49c17fc6a4
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-08-04 19:30:43 +0200

    fix(colorButton): reduce the hover contrast

--- src/shared/components/ColorButton.tsx
+++ src/shared/components/ColorButton.tsx
@@ -17,7 +17,7 @@ function ColorButton({ color, shape, isSelected = false, setTheme }: Props) {
   // TODO: make this accessible
   return (
     <button
-      className={`bg-${color}-500 hover:bg-${color}-300 w-6 h-6 ${buttonShape} ml-2 ${border}`}
+      className={`bg-${color}-500 hover:bg-${color}-400 w-6 h-6 ${buttonShape} ml-2 ${border}`}
       onClick={() => setTheme(color)}
     />
   )
